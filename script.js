17fc410c65b65108baf9c1d4acae6c51ea3825fc
MohamedHerz <mohamed.herz96@gmail.com>
2025-09-24 18:21:49 +0300

    added the logic for timer function and few images for main page to use later

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function StartGame() {
   //loop thru maingrid and active random cells every 2 second
   //everytime an active cell is clicked add points and pass to scoreBoard
 }
-
 function scoreBoard() {
   //store and show scores of current game and previous attempts
   //show green arrow up if the current score is higher than previous attempt, red arrow down if lower
@@ -23,12 +22,19 @@ function timer() {
   //start timer on click of start button for 1min
   //timer font goes red and flash in last 10sec
   //maybe use a tic sound for the time
-
-  for (let i = 0; i >= 60; i++) {
-    //print i
-    //delay 1sec
-    // would this delay stop pause the entire game?
-  }
+  let PlayingTime = 60
+  const Mycounter = setInterval(() => {
+    PlayingTime--
+    if (PlayingTime <= 0) {
+      clearInterval(Mycounter)
+      //switch time text to: time up!
+    }
+    if (PlayingTime <= 10) {
+      //switch time display text to red + UI effect like flashing
+    } else {
+      //display countdown on ui
+    }
+  }, 1000)
 }
 
 //event listeners
